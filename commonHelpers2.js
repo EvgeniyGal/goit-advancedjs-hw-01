9b74abadd067de10e5131039e6f0e4a870b25871
EvgeniyGal <evgeniygal@gmail.com>
2023-11-02 15:14:46 +0000

    Deploying to gh-pages from @ EvgeniyGal/goit-advancedjs-hw-01@26d5351fc90b616f5cd492fe4a498cc8602554ad 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-78be7656.js";const i=document.querySelector("#vimeo-player"),t=new r(i);t.on("timeupdate",a(m,1e3));const e=JSON.parse(localStorage.getItem("videoplayer-current-time"));e&&t.setCurrentTime(e.seconds);function m(o){localStorage.setItem("videoplayer-current-time",JSON.stringify(o))}
+//# sourceMappingURL=commonHelpers2.js.map
